9a8fff1ae445ad20bf1500a32c7fa366cd3a2240
Matt Horn <mathphreak@gmail.com>
2017-01-02 16:43:57 -0700

    Add app update function (#3)
    * feat(update): add app update function
    
    * Fix updateApp() and add tests
    
    * Don't do a stupid
    
    * Lower expectations about nonexistent apps
    
    * Fail differently on nonexistent apps

--- test.js
+++ test.js
@@ -6,6 +6,14 @@ import test from 'ava'
 import del from 'del'
 import steamcmd from './'
 
+test.before(() => {
+  mkdirp.sync('test_data')
+})
+
+test.after.always(() => {
+  return del('test_data')
+})
+
 test.beforeEach(t => {
   const binDirParent = tempfile('/')
   mkdirp.sync(binDirParent)
@@ -40,9 +48,7 @@ test('touch', async t => {
 test('prep', async t => {
   var {binDir, opts} = t.context
   await steamcmd.prep(opts)
-  t.notThrows(() => {
-    fs.statSync(path.join(binDir, 'public'))
-  })
+  t.notThrows(() => fs.statSync(path.join(binDir, 'public')))
 })
 
 test('getAppInfo', async t => {
@@ -64,4 +70,41 @@ test('repeated calls to getAppInfo', async t => {
   const csgoAppInfo = await steamcmd.getAppInfo(730, opts)
   t.is(csgoAppInfo.common.name, 'Counter-Strike: Global Offensive')
   t.notThrows(steamcmd.getAppInfo(730, opts))
+})
+
+test('updateApp with a relative path', async t => {
+  var {opts} = t.context
+  await steamcmd.download(opts)
+  // fix random EBUSY on Windows
+  await new Promise(resolve => setTimeout(resolve, 200))
+  await steamcmd.touch(opts)
+  t.throws(() => steamcmd.updateApp(1007, 'bad_steamworks', opts))
+  t.throws(() => fs.statSync('bad_steamworks'))
+})
+
+test('updateApp with a nonexistent app', async t => {
+  var {opts} = t.context
+  await steamcmd.download(opts)
+  // fix random EBUSY on Windows
+  await new Promise(resolve => setTimeout(resolve, 200))
+  await steamcmd.touch(opts)
+  t.throws(steamcmd.updateApp(4, path.resolve('test_data', 'nonexistent_app'), opts))
+})
+
+test('updateApp with valid parameters', async t => {
+  var {opts} = t.context
+  await steamcmd.download(opts)
+  // fix random EBUSY on Windows
+  await new Promise(resolve => setTimeout(resolve, 500))
+  await steamcmd.touch(opts)
+  t.true(await steamcmd.updateApp(1007, path.resolve('test_data', 'steamworks'), opts))
+})
+
+test('updateApp with HLDS workaround', async t => {
+  var {opts} = t.context
+  await steamcmd.download(opts)
+  // fix random EBUSY on Windows
+  await new Promise(resolve => setTimeout(resolve, 500))
+  await steamcmd.touch(opts)
+  t.true(await steamcmd.updateApp(90, path.resolve('test_data', 'hlds'), opts))
 })
