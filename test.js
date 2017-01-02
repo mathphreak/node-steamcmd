ef9b8c8df3ee114257fadaf898a34754ea55420c
sjenkins7 <sjenkins7@users.noreply.github.com>
2017-01-02 14:31:36 +0000

    Update out-of-date dependencies
    NOTE: Updating these dependencies breaks v0.10 & v0.12, hence the major
    version bump.

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
-import test from 'ava'
-import steamcmd from './'
 import fs from 'fs'
+import path from 'path'
 import tempfile from 'tempfile'
 import mkdirp from 'mkdirp'
-import path from 'path'
+import test from 'ava'
 import del from 'del'
+import steamcmd from './'
 
-test.beforeEach((t) => {
+test.beforeEach(t => {
   const binDirParent = tempfile('/')
   mkdirp.sync(binDirParent)
   const binDir = path.join(binDirParent, 'steamcmd_bin')
@@ -14,11 +14,11 @@ test.beforeEach((t) => {
   t.context = {binDirParent, binDir, opts}
 })
 
-test.afterEach(async (t) => {
+test.afterEach(async t => {
   await del(t.context.binDirParent, {force: true})
 })
 
-test('download', async (t) => {
+test('download', async t => {
   var {binDir, opts} = t.context
   await steamcmd.download(opts)
   t.notThrows(() => {
@@ -26,18 +26,18 @@ test('download', async (t) => {
   })
 })
 
-test('touch', async (t) => {
+test('touch', async t => {
   var {binDir, opts} = t.context
   await steamcmd.download(opts)
   // fix random EBUSY on Windows
-  await new Promise((resolve) => setTimeout(resolve, 200))
+  await new Promise(resolve => setTimeout(resolve, 200))
   await steamcmd.touch(opts)
   t.notThrows(() => {
     fs.statSync(path.join(binDir, 'public'))
   })
 })
 
-test('prep', async (t) => {
+test('prep', async t => {
   var {binDir, opts} = t.context
   await steamcmd.prep(opts)
   t.notThrows(() => {
@@ -45,21 +45,21 @@ test('prep', async (t) => {
   })
 })
 
-test('getAppInfo', async (t) => {
+test('getAppInfo', async t => {
   var {opts} = t.context
   await steamcmd.download(opts)
   // fix random EBUSY on Windows
-  await new Promise((resolve) => setTimeout(resolve, 200))
+  await new Promise(resolve => setTimeout(resolve, 200))
   await steamcmd.touch(opts)
   const csgoAppInfo = await steamcmd.getAppInfo(730, opts)
   t.is(csgoAppInfo.common.name, 'Counter-Strike: Global Offensive')
 })
 
-test('repeated calls to getAppInfo', async (t) => {
+test('repeated calls to getAppInfo', async t => {
   var {opts} = t.context
   await steamcmd.download(opts)
   // fix random EBUSY on Windows
-  await new Promise((resolve) => setTimeout(resolve, 200))
+  await new Promise(resolve => setTimeout(resolve, 200))
   await steamcmd.touch(opts)
   const csgoAppInfo = await steamcmd.getAppInfo(730, opts)
   t.is(csgoAppInfo.common.name, 'Counter-Strike: Global Offensive')
